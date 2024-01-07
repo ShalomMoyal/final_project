8fd3d7d0bf13bc41e6474fdd995f60df8c74b3df
moyal <shalom14294@gmail.com>
2024-01-07 21:16:04 +0200

    update 7/1/24

--- Jscript.js
+++ Jscript.js
@@ -1,25 +1,65 @@
-function getRequest(url){
-    return new Promise((resolve, reject) => {
-        const xmlHttpObj = new XMLHttpRequest();
-        xmlHttpObj.open('GET', url);
-        xmlHttpObj.addEventListener('load', function(){
-            resolve(JSON.parse(this.responseText));
-        });
-        xmlHttpObj.addEventListener('error',function(){
-            reject(this.error);
-        });
-        xmlHttpObj.send();
-    });
-}
-
-getRequest().then(rseult =>{
-
-}).catch(error =>{
-
-})
+const API_Token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGQwZmYxOWJlNzEzYjkyMmQzYWRmZjUyMWM3ZDczNCIsInN1YiI6IjY1ODMzNWY5ZTI5NWI0M2NiYjY4NTMzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SBTase5fJJEEeMpSTLMDBWxp4pY1S7OKVnSIoV5NzHk";
 
 function search() {
     var searchQuery = document.getElementById('query').value;
-
-    
+ 
 }
+function selcetMovie(){
+
+  let value = document.getElementById("genre").value;
+ const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGQwZmYxOWJlNzEzYjkyMmQzYWRmZjUyMWM3ZDczNCIsInN1YiI6IjY1ODMzNWY5ZTI5NWI0M2NiYjY4NTMzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SBTase5fJJEEeMpSTLMDBWxp4pY1S7OKVnSIoV5NzHk'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/popular', options)
+    .then(response => response.json())
+    .then(response => printImg(response))
+    .catch(err => console.error(err));
+
+}
+
+
+function printImg(response){
+    console.log(response);
+    const section = document.querySelector('.container');
+    const box = document.querySelector(".box")
+
+    response.results.forEach(itam => {
+    
+        const div = document.createElement('div');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+        const modal = document.createElement('div');
+        const modalContent = document.createElement('div');
+        const closeBtn = document.createElement('span');
+
+        div.className = 'item';
+        img.src = "https://www.themoviedb.org/t/p/w220_and_h330_face"+ itam.backdrop_path;
+        a.appendChild(img);
+        div.appendChild(a);
+        box.appendChild(div);
+        section.appendChild(box);
+        document.body.appendChild(section);
+    
+    });
+}
+
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGQwZmYxOWJlNzEzYjkyMmQzYWRmZjUyMWM3ZDczNCIsInN1YiI6IjY1ODMzNWY5ZTI5NWI0M2NiYjY4NTMzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SBTase5fJJEEeMpSTLMDBWxp4pY1S7OKVnSIoV5NzHk'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/now_playing', options)
+    .then(response => response.json())
+    .then(response => printImg(response))
+    .catch(err => console.error(err));
+
+
